59722e42355b64e215e9e6eb6d334b515de229f2
Ricardo Gabriel Herdt <rgherdt@inf.ufrgs.br>
2014-01-14 19:28:26 -0200

    defined: delNode; delEdge

--- src/Graph/Digraph.hs
+++ src/Graph/Digraph.hs
@@ -15,15 +15,19 @@ addNode n@(Node id _) g@(Digraph nm em)
     | otherwise = Just $ Digraph (insert id n nm) em
 
 addEdge :: Edge b -> Digraph a b -> Maybe (Digraph a b)
-addEdge e@(Edge id _ _) g@(Digraph nm em)
+addEdge e@(Edge id _ _) (Digraph nm em)
     | id `member` em = Nothing
     | otherwise = Just $ Digraph nm (insert id e em)
 
 delNode :: Node a -> Digraph a b -> Maybe (Digraph a b)
-delNode = undefined
+delNode n@(Node id _) (Digraph nm em)
+    | id `member` nm = Just $ Digraph (delete id nm) em
+    | otherwise = Nothing
 
 delEdge :: Edge b -> Digraph a b -> Maybe (Digraph a b)
-delEdge = undefined
+delEdge e@(Edge id _ _) (Digraph nm em) 
+    | id `member` em = Just $ Digraph nm (delete id em)
+    | otherwise = Nothing
 
 nodes :: Digraph a b -> [Node a]
 nodes = undefined
