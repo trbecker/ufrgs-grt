73b488a5f6b4be577634ffc800dc38cc703580c1
Ricardo Gabriel Herdt <rgherdt@inf.ufrgs.br>
2014-01-16 11:19:50 -0200

    addEdge checks for existence of nodes

--- src/Graph/Digraph.hs
+++ src/Graph/Digraph.hs
@@ -25,20 +25,23 @@ empty = Digraph (Data.IntMap.empty) (Data.IntMap.empty)
 addNode :: (Monad m) => Node a -> Digraph a b -> m (Digraph a b)
 addNode n@(Node id _) g@(Digraph nm em) =
     if id `member` nm 
-        then fail $ "addNode: node " ++ show id ++ " already in Digraph"
+        then fail $ "addNode: node " ++ show id ++ " already in digraph"
         else return $ Digraph (insert id n nm) em
 
 addEdge :: (Monad m) => Edge b -> Digraph a b -> m (Digraph a b)
-addEdge e@(Edge id _ _) g@(Digraph nm em) =
-    if id `member` em 
-        then fail $ "addEdge: edge " ++ show id ++ " already in Digraph"
-        else return $ Digraph nm (insert id e em)
+addEdge e@(Edge id (s, t) _) g@(Digraph nm em)
+    | id `member` em = 
+        fail $ "addEdge: edge " ++ show id ++ " already in digraph"
+    | s `member` nm && t `member` nm =
+        return $ Digraph nm (insert id e em)
+    | otherwise =
+        fail $ "addEdge: edge has nodes not found in digraph"
 
 removeNode :: (Monad m) => Node a -> Digraph a b -> m (Digraph a b)
 removeNode n@(Node id _) g@(Digraph nm em) =
     if id `member` nm 
         then return $ Digraph (delete id nm) em
-        else fail $ "removeNode: node " ++ show id ++ " not in Digraph"
+        else fail $ "removeNode: node " ++ show id ++ " not in digraph"
 
 removeEdge :: (Monad m) => Edge b -> Digraph a b -> m (Digraph a b)
 removeEdge e@(Edge id _ _) g@(Digraph nm em) =
