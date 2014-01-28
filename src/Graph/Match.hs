ce27b801402da7327fe3b7b3dad02d2627956da5
Ricardo Gabriel Herdt <rgherdt@inf.ufrgs.br>
2014-01-28 16:11:36 -0200

    tabs instead of repeated spaces

--- src/Graph/Match.hs
+++ src/Graph/Match.hs
@@ -6,7 +6,7 @@ import qualified Data.IntMap as IM
 
 findMatches :: TypedDigraph a b -> TypedDigraph a b -> [Morphism a b]
 findMatches l g = undefined
-    
+	
 {- | given two Graphs 'l' and 'g', an Edge id from Graph 'l' 'eid' and 
    a Morphism 's' (representing the current state), returns a list of valid 
    Morphisms where the given edge and it's source/target nodes are mapped
@@ -40,37 +40,37 @@ that forces any matching edge to have the corresponding node as it's
 source/target.
 -}
 addEdgeConstraint :: [(Int, Int)] -- already mapped edges [(src, tar)]
-                    -> Int        -- edge id 'lid'
-                    -> TypedDigraph a b -- 'l' graph
-                    -> ([(Int, Int)], EdgeConstraint)
-addEdgeConstraint me lid l@(TypedDigraph (Digraph nm em) _)  = let
-    lEdge@(Just (Edge _ (lsrc, ltar) _)) = IM.lookup lid em
-    lsrcType = getNodeType lsrc l
-    ltarType = getNodeType ltar l
-    srcFound = lsrc `elem` me
-    tarFound = ltar `elem` me
-    checkSrc = if srcFound
-        then (\x -> x == lsrc)
-        else (\x -> True)
-    checkTar = if tarFound
-        then (\x -> x == ltar)
-        else (\x -> True)
-    constraint = (\gid g -> let
-        gEdge@(Just (Edge _ (gsrc, gtar) _)) = IM.lookup gid g
-        gsrcType = getNodeType gsrc g
-        gtarType = getNodeType gtar g
-        in ((lsrcType == gsrcType)
-            && (ltarType == gtarType)
-            && checkSrc
-            && checkTar))
-    in ((lsrc, ltar):me, constraint)
-                
-            
+					-> Int		  -- edge id 'lid'
+					-> TypedDigraph a b -- 'l' graph
+					-> ([(Int, Int)], EdgeConstraint)
+addEdgeConstraint me lid l@(TypedDigraph (Digraph nm em) _)	 = let
+	lEdge@(Just (Edge _ (lsrc, ltar) _)) = IM.lookup lid em
+	lsrcType = getNodeType lsrc l
+	ltarType = getNodeType ltar l
+	srcFound = lsrc `elem` me
+	tarFound = ltar `elem` me
+	checkSrc = if srcFound
+		then (\x -> x == lsrc)
+		else (\x -> True)
+	checkTar = if tarFound
+		then (\x -> x == ltar)
+		else (\x -> True)
+	constraint = (\gid g -> let
+		gEdge@(Just (Edge _ (gsrc, gtar) _)) = IM.lookup gid g
+		gsrcType = getNodeType gsrc g
+		gtarType = getNodeType gtar g
+		in ((lsrcType == gsrcType)
+			&& (ltarType == gtarType)
+			&& checkSrc
+			&& checkTar))
+	in ((lsrc, ltar):me, constraint)
+				
+			
 sameType :: NodeConstraint
 sameType l lid g gid = let
-    lType = getNodeType l lid
-    gType = getNodeType g gid
-    in lType == gType
+	lType = getNodeType l lid
+	gType = getNodeType g gid
+	in lType == gType
 
 -- rewrite rule graph match
 rewrite :: (Monad m) => Rule a b -> TypedDigraph a b -> Morphism a b -> m TypedDigraph a b
