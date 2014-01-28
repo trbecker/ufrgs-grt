62c0d2da0987834838e14a6877d9d5ad43447134
Ricardo Gabriel Herdt <rgherdt@inf.ufrgs.br>
2014-01-28 16:05:36 -0200

    Spaces replaced by tabs (like in the other modules).

--- src/Graph/Digraph.hs
+++ src/Graph/Digraph.hs
@@ -1,19 +1,19 @@
 module Graph.Digraph 
-    ( Edge (..)
-    , Node (..)
-    , Digraph
-    , Morphism (..)
-    , empty
-    , addNode
-    , addEdge
-    , removeNode
-    , removeEdge
-    , keepNode
-    , keepEdge
-    , nodes
-    , edges
-    , applyMorphism
-    ) where
+	( Edge (..)
+	, Node (..)
+	, Digraph
+	, Morphism (..)
+	, empty
+	, addNode
+	, addEdge
+	, removeNode
+	, removeEdge
+	, keepNode
+	, keepEdge
+	, nodes
+	, edges
+	, applyMorphism
+	) where
 
 import Control.Monad
 
@@ -33,47 +33,47 @@ empty = Digraph (IM.empty) (IM.empty)
 
 addNode :: (Monad m) => Node a -> Digraph a b -> m (Digraph a b)
 addNode n@(Node id _) g@(Digraph nm em) =
-    if id `IM.member` nm 
-        then fail $ "addNode: node " ++ show id ++ " already in digraph"
-        else return $ Digraph (IM.insert id n nm) em
+	if id `IM.member` nm 
+		then fail $ "addNode: node " ++ show id ++ " already in digraph"
+		else return $ Digraph (IM.insert id n nm) em
 
 addEdge :: (Monad m) => Edge b -> Digraph a b -> m (Digraph a b)
 addEdge e@(Edge id (s, t) _) g@(Digraph nm em)
-    | id `IM.member` em = 
-        fail $ "addEdge: edge " ++ show id ++ " already in digraph"
-    | s `IM.member` nm && t `IM.member` nm =
-        return $ Digraph nm (IM.insert id e em)
-    | otherwise =
-        fail $ "addEdge: edge points to nodes not found in digraph"
+	| id `IM.member` em = 
+		fail $ "addEdge: edge " ++ show id ++ " already in digraph"
+	| s `IM.member` nm && t `IM.member` nm =
+		return $ Digraph nm (IM.insert id e em)
+	| otherwise =
+		fail $ "addEdge: edge points to nodes not found in digraph"
 
 removeNode :: (Monad m) => Node a -> Digraph a b -> m (Digraph a b)
 removeNode n@(Node id _) g@(Digraph nm em)
-    | id `IM.notMember` nm =
-        fail $ "removeNode: node " ++ show id ++ " not in digraph"
-    | IM.fold 
-        (\(Edge eid (s, t) _) acc -> acc || s == id || t == id) 
-        False em =
-        fail $ "removeNode: node " ++ show id ++ " has some edge pointing to it"
-    | otherwise =
-        return $ Digraph (IM.delete id nm) em
+	| id `IM.notMember` nm =
+		fail $ "removeNode: node " ++ show id ++ " not in digraph"
+	| IM.fold 
+		(\(Edge eid (s, t) _) acc -> acc || s == id || t == id) 
+		False em =
+		fail $ "removeNode: node " ++ show id ++ " has some edge pointing to it"
+	| otherwise =
+		return $ Digraph (IM.delete id nm) em
 
 removeEdge :: (Monad m) => Edge b -> Digraph a b -> m (Digraph a b)
 removeEdge e@(Edge id _ _) g@(Digraph nm em) =
-    if id `IM.member` em 
-        then return $ Digraph nm (IM.delete id em)
-        else fail $ "removeEdge: edge " ++ show id ++ " not in digraph"
+	if id `IM.member` em 
+		then return $ Digraph nm (IM.delete id em)
+		else fail $ "removeEdge: edge " ++ show id ++ " not in digraph"
 
 keepNode :: (Monad m) => Node a -> Digraph a b -> m (Digraph a b)
 keepNode (Node nid _) g@(Digraph ns es) = 
-    if nid `IM.member` ns
-        then return g
-        else fail $ "keepNode: node " ++ show nid ++ " doesn't exist"
+	if nid `IM.member` ns
+		then return g
+		else fail $ "keepNode: node " ++ show nid ++ " doesn't exist"
 
 keepEdge :: (Monad m) => Edge b -> Digraph a b -> m (Digraph a b)
 keepEdge (Edge eid _ _) g@(Digraph ns es) = 
-    if eid `IM.member` es
-        then return g
-        else fail $ "keepEdge: edge " ++ show eid ++ " doesn't exist"
+	if eid `IM.member` es
+		then return g
+		else fail $ "keepEdge: edge " ++ show eid ++ " doesn't exist"
 
 nodes :: Digraph a b -> [(Node a)]
 nodes (Digraph nm _) = map snd $ IM.toList nm
